6f749dcb73ad4ea55916c738df19029b91e552a2
nicovm0209 <119077576+nicovm0209@users.noreply.github.com>
2024-04-21 09:51:43 -0600

    Add files via upload

---
+++ CasoEstudio/Shared/Error.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared
+{
+    public sealed record Error(string code, string description)
+    {
+
+
+        public static Error None = new Error(string.Empty, string.Empty);
+    }
+}
---
+++ CasoEstudio/Shared/Result.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class Result
+    {
+        protected Result(bool isSuccess, Error error)
+        {
+            if (isSuccess && error != Error.None ||
+                isSuccess && error == Error.None)
+            {
+                throw new ArgumentException("Invalid error.", nameof(error));
+            }
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public bool IsSuccess { get; }
+        public Error Error { get; }
+        public bool IsFailure => !IsSuccess;
+
+        public static Result Success() => new(true, Error.None);
+
+        public static Result Failure(Error error) => new(false, error);
+
+        public static Result<T> Success<T>(T value) => new Result<T>(true, Error.None, value);
+
+        public static Result<T> Failure<T>(Error error) => new(false, error, default(T));
+    }
+
+    public sealed class Result<T> : Result
+    {
+        internal Result(bool IsSuccess, Error error, T value)
+            : base(IsSuccess, error)
+        {
+            Value = value;
+        }
+        public T Value { get; }
+    }
+}
